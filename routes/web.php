0c07501dc134603daf9788a7207906a426ed57cb
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-02-28 19:17:20 +0700

    User Page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [HomeController::class,'home']);
 
 Route::prefix('category')->group(function (){
-   Route::get('/foodBeverage', [ProductsController::class,'foodBeverage']);
-   Route::get('/beautyHealth', [ProductsController::class,'beautyHealth']);
-   Route::get('/homeCare', [ProductsController::class,'homeCare']);
-   Route::get('/babyKid', [ProductsController::class,'babyKid']);
+    Route::get('/foodBeverage', [ProductsController::class,'foodBeverage']);
+    Route::get('/beautyHealth', [ProductsController::class,'beautyHealth']);
+    Route::get('/homeCare', [ProductsController::class,'homeCare']);
+    Route::get('/babyKid', [ProductsController::class,'babyKid']);
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
+
