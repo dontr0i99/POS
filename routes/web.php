aacc1c5306f6d3fcf4e6d86233d948480161ba57
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-02-29 20:58:50 +0700

    DB Facade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelCotroller;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserController;
@@ -29,3 +30,5 @@ Route::prefix('category')->group(function (){
 Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
 
 Route::get('/sales', [PenjualanController::class,'sales']);
+
+Route::get('/level', [LevelCotroller::class, 'index']);
