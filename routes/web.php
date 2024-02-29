f7780dc3a7cdd45717fdcf442a532c3ffcb9512d
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-02-29 12:43:54 +0700

    Page Penjualan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -27,3 +28,4 @@ Route::prefix('category')->group(function (){
 
 Route::get('/user/{id}/name/{name}', [UserController::class,'user']);
 
+Route::get('/sales', [PenjualanController::class,'sales']);
