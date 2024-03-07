66248529178afcb6df9407f145bdd93e479940d7
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-03-07 16:04:02 +0700

    Not Found Exception

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,9 +42,13 @@ class UserController extends Controller
         //     abort(404);
         // });
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404); 
+        // });
+
+        // find user data or throwing 404 not found error
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user.user', ['data' => $user]);
     }
 }
