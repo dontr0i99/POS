3eaa9bf340e174894d573f50200e5687b3d30681
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-03-07 13:40:43 +0700

    Add Fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,18 +17,18 @@ class UserController extends Controller
 
     public function index() {
         // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer_1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);// tambahkan data ke tabel m_user
-
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
         ];
-        UserModel::where('username', 'customer_1')->update($data);// tambahkan data ke tabel m_user
+        UserModel::create($data);// tambahkan data ke tabel m_user
+
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username', 'customer_1')->update($data);// tambahkan data ke tabel m_user
 
         // coba akses UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
