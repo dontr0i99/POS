25c7c1c5c61a109da7007ffabc3df3a5c0c7496f
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-03-07 15:44:01 +0700

    Retrieving Single Model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\UserModel;
+use Database\Seeders\UserSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserController extends Controller
 {
@@ -17,13 +19,13 @@ class UserController extends Controller
 
     public function index() {
         // tambah data user dengan Eloquent Model
-        $data = [
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ];
-        UserModel::create($data);// tambahkan data ke tabel m_user
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::create($data);// tambahkan data ke tabel m_user
 
         // $data = [
         //     'nama' => 'Pelanggan Pertama'
@@ -31,7 +33,18 @@ class UserController extends Controller
         // UserModel::where('username', 'customer_1')->update($data);// tambahkan data ke tabel m_user
 
         // coba akses UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user=UserModel::find(1);
+        // $user = UserModel::where('level_id',1)->first();
+        // $user = UserModel::firstWhere('level_id',1);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user.user', ['data' => $user]);
     }
 }
