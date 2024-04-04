0c115dceb5c3bd668bc17b52230f8f5beb7ab468
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-04-04 08:25:59 +0700

    retrieving or creating models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,33 @@ class UserController extends Controller
         // find user data or throwing 404 not found error
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        $user = UserModel::where('level_id', 2)->count();
-        return view('user.user', ['userCount' => $user]);
+        // $user = UserModel::where('level_id', 2)->count();
+        
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+        
+        return view('user.user', ['data' => $user]);
     }   
 }
--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -12,13 +12,13 @@
 
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <th>Jumlah Pengguna</th>
         </tr>
         <tr>
             <td>{{$userCount}}</td>
-        </tr>
-        {{-- <tr>
+        </tr> --}}
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
@@ -29,7 +29,7 @@
             <td>{{$data->username}}</td>
             <td>{{$data->nama}}</td>
             <td>{{$data->level_id}}</td>
-        </tr> --}}
+        </tr>
         {{-- @foreach ($data as $d)
             <tr>
                 <td>{{$d->user_id}}</td>
