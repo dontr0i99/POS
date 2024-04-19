fae69bb23b3768f888723afc79a3958853f39f86
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-04-19 19:17:26 +0700

    CRUD

---
+++ resources/views/user/user_tambah.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tambah User</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <h1>Form Tambah Data User</h1>
+  <form action="/user/tambah_simpan" method="POST">
+    {{-- @csrf --}}
+    {{ csrf_field() }}
+
+    <label>Username</label>
+    <input type="text" name="username" placeholder="Masukkan Username">
+    <br>
+    <label>Nama</label>
+    <input type="text" name="nama" placeholder="Masukkan Nama">
+    <br>  
+    <label>Password</label>
+    <input type="password" name="password" placeholder="Masukkan Password">
+    <br>
+    <label>Level ID</label>
+    <input type="number" name="level_id" placeholder="Masukkan ID Level">
+    <br><br>
+    <input type="submit" class="btn btn-success" value="simpan">
+  </form>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
