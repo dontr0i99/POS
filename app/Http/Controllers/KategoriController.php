cf6bdad28fbf4c7365ea684cd0b539fc9f2edf9f
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-04-20 14:15:57 +0700

    Praktikum 1 – Integrasi Laravel dengan AdminLte3

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -7,22 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index() {
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
+    // public function index() {
+    //     // $data = [
+    //     //     'kategori_kode' => 'SNK',
+    //     //     'kategori_nama' => 'Snack/Makanan Ringan',
+    //     //     'created_at' => now()
+    //     // ];
+    //     // DB::table('m_kategori')->insert($data);
+    //     // return 'Insert data baru berhasil';
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. jumlah data yang diupdate: '. $row . ' baris';
+    //     // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+    //     // return 'Update data berhasil. jumlah data yang diupdate: '. $row . ' baris';
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: '. $row .' baris';
+    //     // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+    //     // return 'Delete data berhasil. Jumlah data yang dihapus: '. $row .' baris';
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori.kategori', ['data' => $data]);
-    }
+    //     $data = DB::table('m_kategori')->get();
+    //     return view('kategori.kategori', ['data' => $data]);
+    // }
+    
 }
