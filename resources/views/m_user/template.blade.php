abe066d0e2642ac63cd02ab5c0591938cb993808
dontr0i99 <118823622+dontr0i99@users.noreply.github.com>
2024-04-20 20:54:46 +0700

    Tugas JS 6

--- resources/views/m_user/template.blade.php
+++ resources/views/m_user/template.blade.php
@@ -1,14 +1,19 @@
+@extends('adminlte::page')
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>CRUD Laravel</title>
-<link rel="stylesheet"
-href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.mi
-n.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD Laravel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha-384-QWTKZyjpPEjISv5WaRU90FeRpok6YmDr5pNlyT2brjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-@yield('content')
-</div>
+    <div class="container">
+        @section('content')
+            @yield('content_body')
+        @endsection
+    </div>
 </body>
 </html>
